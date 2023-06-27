78e295c0b60fe76bda90ad5a401521521c003f5a
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-26 17:23:27 -0700

    restarted the shell project again, working on the mains

---
+++ theshell.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a function that initiates the shell by checking
+ * if it is called in interactive or non-interactive mode
+ * Return: Always (0) on success
+ */
+
+int main(void)
+{
+	if (isatty(STDIN_FILENO) == 0)
+	{
+		non_interactive_mode;
+	}
+	else if (isatty(STDIN_FILENO) == 1)
+	{
+		interactive_mode;
+	}
+
+	return (0);
+}
